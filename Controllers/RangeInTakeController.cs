16d060222e0c1e38641249a760fd19b8401a1518
FatemehRaoufi <Raufi4u@mail.com>
2022-01-05 01:20:23 +0100

    [Fatemeh] Added using Take in Range method

---
+++ Controllers/RangeInTakeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET6LINQImprovements.Models;
+
+namespace NET6LINQImprovements.Controllers
+{
+    public class RangeInTakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<int> numbers = new int[] { 1, 2, 3, 4, 5 };
+            var taken1 = numbers.Take(2..4);
+            string Result = "";
+            foreach (var item in taken1)
+            {
+                Result = Result + item +", ";
+            }
+
+            var viewModel = new ShowResultViewModel
+            {
+                Result1 = Result.ToString(),
+
+
+            };
+            return View(viewModel);
+        }
+    }
+}
